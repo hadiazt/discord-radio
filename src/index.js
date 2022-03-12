e80eef16e6b34f54d38470f42e4c1fed40256a91
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-12 12:33:13 +0330

    Added `Presence: { status, type, name }`

--- src/index.js
+++ src/index.js
@@ -1,79 +1,79 @@
 
 
 module.exports = {
-    ARAB: function ({ channelID, guildID, logID, TOKEN }) {
+    ARAB: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { ARAB } = require('./stations/arab.js')
-        ARAB({ channelID, guildID, logID, TOKEN })
+        ARAB({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    BASS: function ({ channelID, guildID, logID, TOKEN }) {
+    BASS: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { BASS } = require('./stations/bass.js')
-        BASS({ channelID, guildID, logID, TOKEN })
+        BASS({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    DANCE: function ({ channelID, guildID, logID, TOKEN }) {
+    DANCE: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { DANCE } = require('./stations/dance')
-        DANCE({ channelID, guildID, logID, TOKEN })
+        DANCE({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    DISCO: function ({ channelID, guildID, logID, TOKEN }) {
+    DISCO: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { DISCO } = require('./stations/disco.js')
-        DISCO({ channelID, guildID, logID, TOKEN })
+        DISCO({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    ELECTRONIC: function ({ channelID, guildID, logID, TOKEN }) {
+    ELECTRONIC: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { ELECTRONIC } = require('./stations/electronic.js')
-        ELECTRONIC({ channelID, guildID, logID, TOKEN })
+        ELECTRONIC({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    FARDA: function ({ channelID, guildID, logID, TOKEN }) {
+    FARDA: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { FARDA } = require('./stations/farda.js')
-        FARDA({ channelID, guildID, logID, TOKEN })
+        FARDA({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    ILOVE: function ({ channelID, guildID, logID, TOKEN }) {
+    ILOVE: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { ILOVE } = require('./stations/ilove.js')
-        ILOVE({ channelID, guildID, logID, TOKEN })
+        ILOVE({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    JAVAN: function ({ channelID, guildID, logID, TOKEN }) {
+    JAVAN: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { JAVAN } = require('./stations/javan.js')
-        JAVAN({ channelID, guildID, logID, TOKEN })
+        JAVAN({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    JAZZ: function ({ channelID, guildID, logID, TOKEN }) {
+    JAZZ: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { JAZZ } = require('./stations/jazz.js')
-        JAZZ({ channelID, guildID, logID, TOKEN })
+        JAZZ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    KPOP: function ({ channelID, guildID, logID, TOKEN }) {
+    KPOP: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { KPOP } = require('./stations/kpop.js')
-        KPOP({ channelID, guildID, logID, TOKEN })
+        KPOP({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    LOFI: function ({ channelID, guildID, logID, TOKEN }) {
+    LOFI: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { LOFI } = require('./stations/lofi.js')
-        LOFI({ channelID, guildID, logID, TOKEN })
+        LOFI({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    LOFIANIME: function ({ channelID, guildID, logID, TOKEN }) {
+    LOFIANIME: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { LOFIANIME } = require('./stations/lofianime.js')
-        LOFIANIME({ channelID, guildID, logID, TOKEN })
+        LOFIANIME({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    LOFIPANDA: function ({ channelID, guildID, logID, TOKEN }) {
+    LOFIPANDA: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { LOFIPANDA } = require('./stations/lofipanda.js')
-        LOFIPANDA({ channelID, guildID, logID, TOKEN })
+        LOFIPANDA({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    POP: function ({ channelID, guildID, logID, TOKEN }) {
+    POP: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { POP } = require('./stations/pop.js')
-        POP({ channelID, guildID, logID, TOKEN })
+        POP({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
-    ROCK: function ({ channelID, guildID, logID, TOKEN }) {
+    ROCK: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
         const { ROCK } = require('./stations/rock.js')
-        ROCK({ channelID, guildID, logID, TOKEN })
+        ROCK({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } })
     },
 
 }
