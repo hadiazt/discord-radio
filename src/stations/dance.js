e55573a98222b839275c8b11602eec924fdddd2d
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-12 12:33:39 +0330

    Added `Presence`

--- src/stations/dance.js
+++ src/stations/dance.js
@@ -12,17 +12,17 @@ const client = new Client({
 });
 
 module.exports = {
-    DANCE: function ({ channelID, guildID, logID, TOKEN }) {
+    DANCE: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
 
         client.login(TOKEN)
         client.on("ready", () => {
 
             console.log(`Logged in as ${client.user.tag}`);
             client.user.setPresence({
-                status: 'dnd',
+                status: status || 'dnd',
                 activities: [{
-                    type: 'LISTENING',
-                    name: 'DANCE STATION',
+                    type: type || 'LISTENING',
+                    name: name || 'DANCE STATION',
                 }]
             });
 
