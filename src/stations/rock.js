a823aeb5f5351e4d26212e2e5d665a69f7b91ca7
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-12 12:34:31 +0330

    Added `Presence`

--- src/stations/rock.js
+++ src/stations/rock.js
@@ -12,17 +12,17 @@ const client = new Client({
 });
 
 module.exports = {
-    ROCK: function ({ channelID, guildID, logID, TOKEN }) {
+    ROCK: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
 
         client.login(TOKEN)
         client.on("ready", () => {
 
             console.log(`Logged in as ${client.user.tag}`);
             client.user.setPresence({
-                status: 'dnd',
+                status: status || 'dnd',
                 activities: [{
-                    type: 'LISTENING',
-                    name: 'ROCK STATION',
+                    type: type || 'LISTENING',
+                    name: name || 'ROCK STATION',
                 }]
             });
 
