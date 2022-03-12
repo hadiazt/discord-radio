28fd25ee54cfd2f484a38993331293f7a1ab87ca
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-12 12:33:34 +0330

    Added `Presence`

--- src/stations/arab.js
+++ src/stations/arab.js
@@ -12,17 +12,17 @@ const client = new Client({
 });
 
 module.exports = {
-    ARAB: function ({ channelID, guildID, logID, TOKEN }) {
+    ARAB: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name }  }) {
 
         client.login(TOKEN)
         client.on("ready", () => {
 
             console.log(`Logged in as ${client.user.tag}`);
             client.user.setPresence({
-                status: 'dnd',
+                status: status || 'dnd',
                 activities: [{
-                    type: 'LISTENING',
-                    name: 'ARAB STATION',
+                    type: type || 'LISTENING',
+                    name: name || 'ARAB STATION',
                 }]
             });
 
