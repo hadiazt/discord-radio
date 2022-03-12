0df9afdf03ae0537ef29272e52044a381bef00a2
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-12 12:34:22 +0330

    Added `Presence`

--- src/stations/lofi.js
+++ src/stations/lofi.js
@@ -12,17 +12,17 @@ const client = new Client({
 });
 
 module.exports = {
-    LOFI: function ({ channelID, guildID, logID, TOKEN }) {
+    LOFI: function ({ channelID, guildID, logID, TOKEN, Presence: { status, type, name } }) {
 
         client.login(TOKEN)
         client.on("ready", () => {
 
             console.log(`Logged in as ${client.user.tag}`);
             client.user.setPresence({
-                status: 'dnd',
+                status: status || 'dnd',
                 activities: [{
-                    type: 'LISTENING',
-                    name: 'LOFI STATION',
+                    type: type || 'LISTENING',
+                    name: name || 'LOFI STATION',
                 }]
             });
 
